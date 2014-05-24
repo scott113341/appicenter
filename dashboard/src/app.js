16ccec0a800f7ac6d921ee28c8c66e7d3ce6306a
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-05-24 16:44:11 -0700

    lib

--- dashboard/src/app.js
+++ dashboard/src/app.js
@@ -4,5 +4,15 @@ var angularfire = require('angularfire-browserify');
 
 var app = angular.module('app', ['firebase','ngAnimate'])
 
+function MyController($scope, $firebase) {
+var ref = new Firebase("https://lksg2hcetqi.firebaseio-demo.com/");
+$scope.messages = $firebase(ref);
+$scope.addMessage = function(e) {
+  if (e.keyCode != 13) return;
+  $scope.messages.$add({from: $scope.name, body: $scope.msg});
+  $scope.msg = "";
+};
+}
+
 console.log('hello');
 
