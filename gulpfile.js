06d969c09f388526de2f3b7e336ed348481e7e4a
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-05-24 15:39:18 -0700

    basic setup

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var angular = require('angular');
+var less = require('gulp-less');
+var jade = require('gulp-jade');
+var source = require('vinyl-source-stream');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var prefix = require('gulp-autoprefixer');
+
+var handleErrors = function (error) {
+ 	return 'Error on line' + error.line + ' :' + error.message;
+}
+
+gulp.task('scripts', function() {
+  browserify('./dashboard/src/app.js')
+  .bundle({debug: true})
+  .on('error', notify.onError(handleErrors))
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('./dashboard/build/'));
+
+});
+
+gulp.task('less', function() {
+  gulp.src('./dashboard/src/style.less')
+  	.pipe(less({ compress: true }))
+  	.on('error', notify.onError(handleErrors))
+    .pipe(prefix('last 2 Chrome versions'))
+  	.pipe(gulp.dest('./dashboard/build/'));
+});
+
+gulp.task('jade', function() {
+
+	gulp.src('./dashboard/src/layout.jade')
+		.pipe(jade())
+		.on('error', notify.onError(handleErrors))
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('./dashboard/build/'));
+
+  gulp.src('./src/newtab/templates/*.jade')
+    .pipe(jade())
+    .on('error', notify.onError(handleErrors))
+    .pipe(gulp.dest('./dashboard/build/templates/'));
+
+});
+
+gulp.task('default', ['scripts', 'less', 'jade', 'watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.js', ['scripts']);
+  gulp.watch('./src/**/*.jade', ['jade']);
+  gulp.watch('./src/**/*.less', ['less']);
+});
