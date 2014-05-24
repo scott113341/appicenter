6ffef63eff1663dae6d52b1bd18cbff71b3de42c
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-05-24 16:03:31 -0700

    boilerplate updates

--- dashboard/src/app.js
+++ dashboard/src/app.js
@@ -1,1 +1,10 @@
 var angular = require('angular');
+var firebase = require('client-firebase');
+var angularfire = require('angularfire-browserify');
+
+var app = angular.module('app', ['firebase','ngAnimate'])
+
+
+
+console.log('hello');
+
