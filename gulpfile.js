46cd73a653f0ff13754fc3047f9262fd47693353
Scott Hardy <scott.the.hardy@gmail.com>
2014-05-24 20:11:40 -0700

    added boostrap with bower, refactored to use those less files instead of from the /lib folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ var handleErrors = function (error) {
 gulp.task('scripts', function() {
   gulp
     .src([
+      './bower_components/jquery/dist/jquery.js',
+      './bower_components/bootstrap/dist/js/bootstrap.js',
       './bower_components/angular/angular.js',
       './bower_components/angular-route/angular-route.js',
       './bower_components/firebase/firebase.js',
