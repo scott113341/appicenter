146b96ca277c9d580917dae8df2dd3ef1d54b390
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-05-25 13:06:25 -0700

    scoreboard jank

--- app/src/controllers/ScoreboardCtrl.js
+++ app/src/controllers/ScoreboardCtrl.js
@@ -1,9 +1,48 @@
-appicenter.controller('ScoreboardCtrl', ['$scope', 'firebaseService', function($scope, firebaseService) {
+appicenter.controller('ScoreboardCtrl', ['$scope', '$route', '$timeout', 'firebaseService', function($scope, $route, $timeout, firebaseService) {
   var scoreboardRef = firebaseService('/scoreboard');
   scoreboardRef.$bind($scope, 'scoreboard');
 
 
-  var auctionRef = firebaseService('/auctions');
-  auctionRef.$bind($scope, 'auctions');
+  var auctionsRef = firebaseService('/auctions');
+  auctionsRef.$bind($scope, 'auctions');
+
+
+   // get the current auction's id from the scoreboard
+  var currentAuctionRef = firebaseService('/scoreboard/current_auction');
+  currentAuctionRef.$bind($scope, 'currentAuction').then(function() {
+
+    // reload page if current action id changes
+    currentAuctionRef.$on('change', function() {
+      $route.reload();
+    });
+
+    // get the current auction
+    var auctionsRef = firebaseService('/auctions');
+    var auctionRef = auctionsRef.$child($scope.currentAuction);
+
+    auctionRef.$bind($scope, 'auction').then(function() {
+      $scope.updateProgress();
+    });
+  });
+
+
+  $scope.updateProgress = function() {
+    var auction = $scope.auction;
+    var percent = (Date.now() - auction.start_time) / auction.duration * 100;
+    var time_remaining = (auction.duration - auction.duration * percent / 100) / 1000;
+
+    $scope.progress = {
+      percent: percent.toFixed(2) + '%',
+      time_remaining: Math.floor(time_remaining)
+    };
+
+    if (auction.start_time == 0) $scope.status = 0;
+    else if (time_remaining > 0) $scope.status = 1;
+    else if (time_remaining <= 0) $scope.status = 2;
+
+    $timeout(function() {
+      $scope.updateProgress();
+    }, 1000);
+  };
 
 }]);
